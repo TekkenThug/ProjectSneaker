d69564cc4b4ce690fe4930c68d229efd7909159b
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 01:30:28 +0300

    Initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
-
+    <navbar />
+    <sidebar class="sidebar" />
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 export default {
   name: 'App',
+  components: { Navbar, Sidebar }
 }
 </script>
 
 <style>
-
+  .sidebar {
+    position: fixed;
+    min-height: 400px;
+    margin: 10px;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__logo">
+      <img src="" alt="logotype">
+    </div>
+    <ul class="navbar__items">
+      <li><a href="">First</a></li>
+      <li><a href="">First</a></li>
+      <li><a href="">First</a></li>
+      <li><a href="">First</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    padding: 15px 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 3px solid $orange;
+
+    &__items {
+      display: flex;
+      font-size: 18px;
+
+      li {
+        &:not(&:first-child) {
+          margin-left: 30px;
+        }
+      }
+    }
+  }
+</style>
