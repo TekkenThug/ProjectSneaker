29626bda4296b465b802e349039ad0ef22e0b2e1
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 15:04:47 +0300

    Update app vue

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,27 @@
 <template>
   <div id="app">
-    <navbar />
-
-    <div class="start">
-      <h1 class="start__title">Откройте для себя мир сникеров</h1>
-      <p class="start__subtitle">
-        Смотрите, создавайте и изучайте историю кроссовок вместе с нами
-      </p>
-      <search-field
-          class="start__search"
-          placeholder="Какие кроссовки интересуют?"
-          :icon="searchIcon"
-          v-model="results"
-      />
-
-      {{ results }}
-    </div>
+    <navbar :routes="routes" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import {routesList} from "@/configs/router";
+
 import Navbar from "@/components/Navbar";
-import SearchField from "@/components/UI/Input";
 
 export default {
   name: 'App',
-  components: { Navbar, SearchField },
+  components: { Navbar },
 
   data() {
     return {
-      searchIcon: {
-        width: 18,
-        height: 18,
-        name: 'search'
-      },
-
-      results: '',
+      routes: routesList
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .start {
-    padding: 40px;
-    text-align: center;
 
-    &__title {
-      font-size: 36px;
-      font-weight: 700;
-      margin-bottom: 20px;
-    }
-
-    &__subtitle {
-      color: rgba($gray, .8);
-    }
-
-    &__search {
-      max-width: 500px;
-      margin: 35px auto;
-
-      .svg-icon--search {
-        fill: red;
-        color: red;
-      }
-    }
-  }
 </style>
