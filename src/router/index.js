819d3c013d5b7ab8c5148f722b45e282e75865de
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 18:54:44 +0300

    Update titles on pages

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,15 @@ const routes = [
     path: '/',
     redirect: {
       name: 'Wiki'
-    }
+    },
   },
   {
     path: '/wiki',
     name: 'Wiki',
-    component: Wiki
+    component: Wiki,
+    meta: {
+      title: 'Wiki'
+    }
   },
 ]
 
@@ -23,6 +26,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+});
+
+const generateTitle = (title) => `ProjectSneaker_${title}`
+
+router.beforeEach((to, from, next) => {
+  document.title = generateTitle(to.meta.title);
+  next()
 })
 
 export default router
