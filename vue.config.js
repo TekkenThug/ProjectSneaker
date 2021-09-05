1fb6aee67f24905b494b25eb6e6c2f8735167481
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 13:23:10 +0300

    Add webpack config for svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,11 @@ module.exports = {
             },
         },
     },
+
+    chainWebpack: config => {
+      config.module
+        .rule('svg-sprite')
+        .use('svgo-loader')
+        .loader('svgo-loader')
+    }
 }
