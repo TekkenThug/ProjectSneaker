9ce912e11ea62df714b537b8e61c1a4983d67868
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 16:06:27 +0300

    Add mock data for testing

---
+++ src/mock/sneakers.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        image: '/images/tmp-sneaker.jpeg',
+        name: 'Adidas Streetball',
+        vendorCode: '',
+        averagePrice: 12000
+    },
+    {
+        image: '/images/tmp-sneaker.jpeg',
+        name: 'Nike Blazer',
+        vendorCode: '',
+        averagePrice: 7999
+    },
+    {
+        image: '/images/tmp-sneaker.jpeg',
+        name: 'Reebok Instapump',
+        vendorCode: '',
+        averagePrice: 11990
+    },
+]
