ab9d9026300adbc6eafbf043577cb21c27b015df
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 17:40:55 +0300

    Add styles for preloader

--- src/components/UI/_Preloader/Preloader.vue
+++ src/components/UI/_Preloader/Preloader.vue
@@ -1,12 +1,15 @@
 <template>
-  <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"
-       xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="48" height="48" viewBox="0 0 128 128"
-       xml:space="preserve">
-    <g>
-      <path d="M64 9.75A54.25 54.25 0 0 0 9.75 64H0a64 64 0 0 1 128 0h-9.75A54.25 54.25 0 0 0 64 9.75z" fill="#f67571"/><animateTransform
-      attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1800ms" repeatCount="indefinite"/>
-    </g>
-  </svg>
+  <div class="preloader">
+    <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"
+         xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="48" height="48" viewBox="0 0 128 128"
+         xml:space="preserve">
+      <g>
+        <path d="M64 9.75A54.25 54.25 0 0 0 9.75 64H0a64 64 0 0 1 128 0h-9.75A54.25 54.25 0 0 0 64 9.75z" fill="#f67571"/><animateTransform
+          attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1800ms" repeatCount="indefinite"/>
+      </g>
+    </svg>
+  </div>
+
 </template>
 
 <script>
@@ -16,5 +19,8 @@ export default {
 </script>
 
 <style scoped>
-
+  .preloader {
+    display: flex;
+    justify-content: center;
+  }
 </style>
