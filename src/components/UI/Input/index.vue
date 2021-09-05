21c96b04cdc196ab7aa62f434c9c06557cbc2c28
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-05 14:39:14 +0300

    Create input component

---
+++ src/components/UI/Input/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+      class="ui-input"
+      :class="{
+        'ui-input--focused': focus
+      }"
+  >
+    <icon
+        class="ui-input__icon"
+        v-if="icon"
+        :icon-data="icon"
+    />
+    <input
+        :value="value"
+        :placeholder="placeholder"
+        @input="$emit('input', $event.target.value)"
+        @focusin="focus = true"
+        @focusout="focus = false"
+        type="text"
+    >
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/UI/Icon";
+
+export default {
+  name: "index",
+  components: { Icon },
+
+  data() {
+    return {
+      focus: false,
+    }
+  },
+
+  props: {
+    value: {
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: Object,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .ui-input {
+    @include trans;
+
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+
+    &::after {
+      @include trans;
+
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      z-index: -1;
+      border: 3px solid $orange;
+      border-radius: 7px;
+      opacity: 0;
+    }
+
+    &--focused {
+      &::after {
+        opacity: 1;
+      }
+    }
+
+    input {
+      @include shadowUI;
+
+      padding: 8px 15px;
+      border-radius: 7px;
+      background-color: transparent;
+      border: 1px solid rgba($gray, .7);
+      flex-grow: 1;
+
+      &:focus {
+        outline: none;
+      }
+
+      &::placeholder {
+        opacity: .5;
+      }
+    }
+
+    &__icon {
+      position: absolute;
+      left: 10px;
+      top: 50%;
+      transform: translateY(-50%);
+
+      + input {
+        padding-left: 35px;
+      }
+    }
+  }
+</style>
