a8a4af5c7c958b23415c54f786b9447bb1ff3730
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-07 21:07:48 +0300

    Install and connect api plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './styles/tailwind.scss'
+
+/** Vue tools **/
 import router from './router'
 
+/** Plugins **/
+import apiPlugin from "@/plugins/apiPlugin";
+
+Vue.use(apiPlugin);
+
+/** Global components **/
 import Preloader from "@/components/UI/_Preloader/Preloader";
 
 Vue.component('Preloader', Preloader)
