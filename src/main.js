11e944fcda930f1ae96dbacd4c8693d62a9ba4f9
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-08 10:13:21 +0300

    Add jsdoc

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import './styles/tailwind.scss';
 
-/** Vue tools * */
+/** Vue tools */
 import router from './router';
 
-/** Plugins * */
+/** Plugins */
 import apiPlugin from '@/plugins/apiPlugin';
 
-/** Global components * */
+/** Global components */
 import Preloader from '@/components/UI/_Preloader/Preloader';
 
 Vue.use(apiPlugin);
--- src/services/api/sneakers.js
+++ src/services/api/sneakers.js
@@ -1,5 +1,10 @@
 import nodeAPI from '@/configs/axios';
 
+/**
+ * Get list of sneakers
+ *
+ * @returns {Promise} List of sneakers
+ */
 export default function getSneakers() {
   return nodeAPI.get('/sneakers');
 }
