e4144ba184d706fc1d521f0af46af2e7539f7001
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-11 15:11:39 +0300

    Add typedef of sneakers in api doc

--- src/services/api/sneakers.js
+++ src/services/api/sneakers.js
@@ -1,6 +1,16 @@
 import nodeAPI from '@/configs/axios';
 
 /**
+ * Pair of Sneakers
+ *
+ * @typedef {object} Sneakers
+ * @property {string} name - pair name
+ * @property {string} colorway - current colorway
+ * @property {string} vendorCode - vendor code of product
+ * @property {string} releaseDate - worldwide release date
+ */
+
+/**
  * Get list of sneakers
  *
  * @returns {Promise} List of sneakers
@@ -10,9 +20,9 @@ export function getSneakers() {
 }
 
 /**
- * Post pair of sneakers
+ * Post sneakers
  *
- * @param {object} data - data on a pair of sneakers
+ * @param {Sneakers} data - data on a pair of sneakers
  * @returns {Promise} Status of posting
  */
 export function postSneakers(data) {
