bec4f85c20d8aa3cc2f4f61875bb802605b5eb0c
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-18 16:07:38 +0300

    Create template resolving for drag&drop

--- src/components/Drag&Drop/index.vue
+++ src/components/Drag&Drop/index.vue
@@ -2,25 +2,38 @@
   <label
     class="drag"
     :class="{
-      'drag--entered': entered,
+      'drag--entered': entered || fileName,
     }"
-    @dragenter.prevent="enterHandler(true)"
-    @dragleave.prevent="enterHandler(false)"
+    @dragenter.prevent="entered = true"
+    @dragleave.prevent="entered = false"
     @dragover.prevent
+    @drop.prevent="fileHandler('drop', $event)"
   >
-    <div class="drag__default">
+    <input
+      @change="fileHandler('input', $event)"
+      class="drag__input"
+      type="file"
+    >
+    <span v-if="fileName">
+      {{ fileName }}
+    </span>
+    <span
+      v-else
+      class="drag__default"
+    >
       <icon
         class="drag__default-icon"
         :icon-data="iconData"
       />
-      <h2 class="drag__default-title">
+      <b class="drag__default-title">
         {{ title }}
-      </h2>
-    </div>
-    <input class="drag__input" type="file">
+      </b>
+    </span>
   </label>
 </template>
 <script>
+import { validateImage } from '@/services/validate';
+
 import Icon from '@/components/UI/Icon';
 
 export default {
@@ -40,20 +53,24 @@ export default {
         name: 'image-add',
       },
       entered: false,
+      fileName: null,
     };
   },
   methods: {
-    testDrag(event) {
-      console.log(event);
-    },
+    fileHandler(type, event) {
+      let uploadFile = null;
 
-    dropHandler(event) {
-      console.log(event);
-    },
+      if (type === 'input') {
+        uploadFile = event.target.files[0];
+      } else if (type === 'drop') {
+        uploadFile = event.dataTransfer.files[0];
+      } else {
+        return;
+      }
 
-    enterHandler(value) {
-      console.log(value);
-      this.entered = value;
+      if (validateImage(uploadFile.type)) {
+        this.fileName = uploadFile.name;
+      }
     },
   },
 };
@@ -97,6 +114,10 @@ export default {
       background-color: $color-4;
       color: $color-2;
       border-style: solid;
+
+      &:hover {
+        border-color: $color-4;
+      }
     }
   }
 </style>
