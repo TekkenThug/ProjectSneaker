30b002a7fa24e0ec2d3abb974aadefaa05c89ef1
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-26 11:55:49 +0300

    Create main page view

---
+++ src/views/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <navbar :routes="routes" />
+    <div class="main__view">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import routesList from '@/configs/router';
+
+import Navbar from '@/components/Navbar';
+
+export default {
+  name: 'Main',
+  components: { Navbar },
+  data() {
+    return {
+      routes: routesList,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.main {
+  padding-top: $navbarHeight;
+}
+</style>
