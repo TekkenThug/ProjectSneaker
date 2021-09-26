d398df6753ace277a444f19871d7bc6df425b6ab
Vadim Ignatov <ignatov0131@gmail.com>
2021-09-26 14:56:48 +0300

    Refactoring sign in component

--- src/views/Auth/subviews/SignIn.vue
+++ src/views/Auth/subviews/SignIn.vue
@@ -1,23 +1,15 @@
 <template>
   <div class="sign-in">
-    <img
-      class="sign-in__logo"
-      src="/images/logo.png"
-      alt="logotype"
-    >
     <form
       class="sign-in__form"
       @submit.prevent
     >
       <input-field
+        v-for="(field, index) in fields"
+        :key="index"
         class="sign-in__form-field"
-        v-bind="loginField"
-        v-model="loginField.value"
-      />
-      <input-field
-        class="sign-in__form-field"
-        v-bind="passwordField"
-        v-model="passwordField.value"
+        v-bind="field"
+        v-model="field.value"
       />
       <btn
         class="sign-in__form-submit"
@@ -42,23 +34,25 @@ export default {
   components: { InputField, Btn },
   data() {
     return {
-      loginField: {
-        value: '',
-        placeholder: this.$t('Login'),
-        icon: {
-          width: 16,
-          height: 16,
-          name: 'user',
+      fields: {
+        loginField: {
+          value: '',
+          placeholder: this.$t('Email'),
+          icon: {
+            width: 16,
+            height: 16,
+            name: 'email',
+          },
         },
-      },
-      passwordField: {
-        value: '',
-        type: 'password',
-        placeholder: this.$t('Password'),
-        icon: {
-          width: 16,
-          height: 16,
-          name: 'lock',
+        passwordField: {
+          value: '',
+          type: 'password',
+          placeholder: this.$t('Password'),
+          icon: {
+            width: 16,
+            height: 16,
+            name: 'lock',
+          },
         },
       },
     };
@@ -68,26 +62,6 @@ export default {
 
 <style lang="scss" scoped>
 .sign-in {
-  &__form {
-    margin-top: 40px;
-    padding: 20px;
-    border: 1px solid $color-3;
-    border-radius: 8px;
-
-    &-field {
-      &:not(&:last-child) {
-        margin-bottom: 15px;
-      }
-    }
-
-    &-link {
-      @include linkStyle;
-    }
-
-    &-submit {
-      width: 100%;
-      margin-bottom: 10px;
-    }
-  }
+  @include authForm;
 }
 </style>
