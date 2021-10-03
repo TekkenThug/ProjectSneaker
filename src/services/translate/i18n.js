a7eb0f1f259b5dc302ea3e28648295ab9920bf8b
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-03 16:00:46 +0300

    Refactoring translations dictionary

--- src/services/translate/i18n.js
+++ src/services/translate/i18n.js
@@ -1,23 +1,46 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
+import VMEN from 'vee-validate/dist/locale/en';
+import VMRU from 'vee-validate/dist/locale/ru';
+
 Vue.use(VueI18n);
 
+const DICTIONARY = {};
+
 /**
+ * Set translations from libraries and packages
  *
+ * @param {string}key - key for set translations
+ * @param {JSON} libForEn - translations on english
+ * @param {JSON} libForRu - translations on russian
  */
-function loadLocaleMessages() {
+const processTranslationsFromLib = (key, libForEn, libForRu) => {
+  DICTIONARY.en[key] = libForEn;
+  DICTIONARY.ru[key] = libForRu;
+};
+
+/**
+ * Loading local messages from project
+ *
+ * @returns {object} dictionary with translations
+ */
+const loadLocaleMessages = () => {
   const locales = require.context('../../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages = {};
+
   locales.keys().forEach((key) => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key);
+      DICTIONARY[locale] = locales(key);
     }
   });
-  return messages;
-}
+
+  processTranslationsFromLib('validations', VMEN, VMRU);
+
+  return DICTIONARY;
+};
 
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
