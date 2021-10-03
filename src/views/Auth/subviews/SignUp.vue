ac5aa5fe37988a618235e12bd158c86e410973a9
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-03 16:41:59 +0300

    Validate in auth form for sign up

--- src/views/Auth/subviews/SignUp.vue
+++ src/views/Auth/subviews/SignUp.vue
@@ -21,20 +21,24 @@ export default {
         emailField: {
           value: '',
           placeholder: this.$t('Email'),
+          validationRules: ['required', 'email'],
         },
         nickField: {
           value: '',
           placeholder: this.$t('Nickname'),
+          validationRules: ['required'],
         },
         passwordField: {
           value: '',
           type: 'password',
           placeholder: this.$t('Password'),
+          validationRules: ['required'],
         },
         repeatPasswordField: {
           value: '',
           type: 'password',
           placeholder: this.$t('Repeat password'),
+          validationRules: ['required'],
         },
       },
     };
