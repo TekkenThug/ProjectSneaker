5ddda8fb152e17feab641504dc23d5e892d4b6dd
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-10 16:47:40 +0300

    Create request view

---
+++ src/views/Admin/Requests/Requests.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="requests">
+    <request-item
+      v-for="(pair, key) in pairs"
+      :key="key"
+      :sneaker-data="pair"
+    />
+  </div>
+</template>
+
+<script>
+import RequestItem from '@/components/RequestItem';
+
+export default {
+  name: 'Requests',
+  components: { RequestItem },
+  data() {
+    return {
+      pairs: null,
+    };
+  },
+  mounted() {
+    this.$api.getSneakers().then((res) => {
+      this.pairs = res;
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+//.requests {
+//  display: flex;
+//  justify-content: space-between;
+//}
+</style>
