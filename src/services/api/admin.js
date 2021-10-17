426f3e81d3fe4252e3ce9fc80cde6e7c064ed8ea
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-17 22:23:08 +0300

    Add method for send resolving in api

--- src/services/api/admin.js
+++ src/services/api/admin.js
@@ -9,6 +9,18 @@ export function getNotApprovedSneakers() {
   return nodeAPI.get('/admin/sneakers').then((res) => res.data);
 }
 
+/**
+ * Send a request for acceptance / cancellation of the application
+ *
+ * @param {string} id - identifier of sneakers
+ * @param {boolean} resolve - status of resolve
+ * @returns {Promise} Status of resolving
+ */
+export function approveOrRejectSneakers(id, resolve) {
+  return nodeAPI.post('/admin/sneakers/resolve', { id, resolve }).then((res) => res.data);
+}
+
 export default {
   getNotApprovedSneakers,
+  approveOrRejectSneakers,
 };
