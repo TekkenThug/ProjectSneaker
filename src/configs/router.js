8b3af82b2e865a16982caa13fa5024acb1ee298e
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-17 22:43:11 +0300

    Add routes in config file

--- src/configs/router.js
+++ src/configs/router.js
@@ -5,6 +5,12 @@ export default [
   },
   {
     name: 'Requests',
+    auth: true,
     link: '/admin/requests',
+  },
+  {
+    name: 'Logout',
+    auth: true,
+    link: '/auth/logout',
   },
 ];
