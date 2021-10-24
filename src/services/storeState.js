baa3d86542d6f317a1f9e78a0702ccec1898eaa9
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-24 19:33:14 +0300

    Add method for update tokens

--- src/services/storeState.js
+++ src/services/storeState.js
@@ -2,4 +2,6 @@ import store from '@/store/store';
 
 export default {
   getToken: () => store.state.auth.token,
+  getRefreshToken: () => store.state.auth.refreshToken,
+  updateTokens: () => store.dispatch('auth/refresh'),
 };
