959414e830a4c0d68943c9645f4cb131948f8a33
Vadim Ignatov <ignatov0131@gmail.com>
2021-10-24 19:57:10 +0300

    Rework axios instance object for auth requests/responses

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,5 +1,34 @@
 import axios from 'axios';
+import storeState from '@/services/storeState';
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
+
+const useTokenInRequests = (config) => {
+  const token = storeState.getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+};
+
+const handlerForTokens = async (e) => {
+  if (
+    !storeState.getRefreshToken()
+    || e.response.status !== 401
+    || e.config.retry
+  ) throw e;
+
+  await storeState.updateTokens();
+
+  const newRequest = {
+    ...e.config,
+    retry: true,
+  };
+
+  return axiosInstance(newRequest);
+};
+
+axiosInstance.interceptors.request.use(useTokenInRequests);
+axiosInstance.interceptors.response.use((res) => res, handlerForTokens);
+
+export default axiosInstance;
