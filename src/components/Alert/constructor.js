b6888b6d48caad4d6f55d248a144cb39679e9108
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-22 14:56:11 +0300

    Alert constructor cleaning

--- src/components/Alert/constructor.js
+++ src/components/Alert/constructor.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
 import Alert from './index';
 
+/** Alert instance */
 const AlertClass = Vue.extend(Alert);
 
+/**
+ * Create alert instance and mount it
+ * @param {string} msgType - message type
+ * @param {string} title - alert title
+ * @param {string} text - alert text
+ * @returns {void}
+ */
 export default (msgType, title, text = '') => {
   const alertInstance = new AlertClass({
     propsData: {
